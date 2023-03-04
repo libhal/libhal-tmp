79efb60f73ddf1c52a0030636fd66742c7477956
Khalil Estell <kammcecorp@gmail.com>
2023-03-04 06:31:28 -0800

    :construction_worker: Migrate to new ci repo

---
+++ demos/conanfile.py
@@ -0,0 +1,22 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+
+
+class PcaDemos(ConanFile):
+    settings = "compiler", "build_type"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualBuildEnv"
+
+    def requirements(self):
+        self.requires("libhal-lpc40xx/[^1.0.0]")
+        self.requires("libhal-util/[^1.0.0]")
+        self.requires("libhal-tmpxxx/0.4.0")
+        self.tool_requires("gnu-arm-embedded-toolchain/11.3.0")
+        self.tool_requires("cmake-arm-embedded/0.1.1")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
